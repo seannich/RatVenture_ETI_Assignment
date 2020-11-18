02ef3512e660d510dd72bef0e37cb07d9add1c00
seannich <seannts9@gmail.com>
2020-11-18 17:29:13 +0800

    Update readme.py

--- readme.py
+++ readme.py
@@ -1,1 +1,62 @@
 # created repo
+
+# USER STORIES
+#=========================================================================================================================================================================================
+# NO.           FEATURE                 SUB FEATURE           USER STORY
+# 1             Display Main Menu       New Game              As a player I want to be able to start a new game so that I can play the game.
+#                                       Resume Game           As a player I want to be able to resume from where I left off in the game so that I do not lose progress.
+#                                       Exit Game             As a player I want to be able to have the option to quit so that I can exit the game.
+
+# 2             Display Town Menu       View Character        As a player I want to be able to view my character’s statistics so that I can decide whether to rest or move.
+#                                       View Map              As a player I want to be able to view my character on the map so that I can plan out my next move..
+#                                       Move                  As a player I want to be able to leave the town so that I can progress to the end.
+#                                       Rest                  As a player I want to be able to rest so that I can restore my health.
+#                                       Save Game             As a player I want to be able to save the game so that I can pause and come back to the game another time.
+#                                       Exit Game             As a player I want to be able to exit the game so that I can stop playing and come back another time.
+
+# 3             Combat Menu             Attack                As a player I want to be able to attack during a fight so that I can defeat the opponent.
+#                                       Run                   As a player I want to be able to run away from a fight so that I can prevent myself from dying if I am low on health.
+
+# 4             Display Outdoor Menu    View Character        As a player I want to be able to look at my character’s health so that I can decide to progress or head to a town to rest.
+#                                       View Map              As a player I want to be able to view my character on the map so that I can plan out my next move.
+#                                       Move                  As a player I want to be able to move in the game so that I can advance to the end.
+#                                       Exit Game             As a player I want to be able to exit the game so that I can stop playing and come back another time.          
+#=========================================================================================================================================================================================
+
+# METHODLOGOIES ADOPTED
+# ==========================================
+# NO.           MODEL
+# 1             Iterative Model
+# 2             Continuous Integration
+# 3             DevOps
+# 4             Agile
+# ==========================================
+
+# PRODUCT BACKLOG
+# =====================================================================
+# SPRINT NO.    TASKS               
+# 1             UI Creation
+#               1.  Features
+#               2.  Sub features
+#               3.  World Map
+
+# 2             Connecting UI
+#               1.  Linking sub features with other features 
+#               2.  Linking sub features with other sub features
+
+# 3             Functions pt.1
+#               1.  Character Movement
+#               2.  Rest
+#               3.  Display Stats
+#               4.  View Map
+
+# 4            Functions pt.2
+#               1.  Attack
+#               2.  Run
+#               3.  Save
+#               4.  Resume
+#               5.  Quit
+# =====================================================================
+#
+#
+#
