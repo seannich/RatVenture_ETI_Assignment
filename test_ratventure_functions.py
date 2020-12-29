6296d86356b02ae0f843b703de91ec37f3d40370
adele <adelinew06@gmail.com>
2020-12-29 16:23:51 +0800

    Test Script - Attack Menu & Outdoor Menu

--- test_ratventure_functions.py
+++ test_ratventure_functions.py
@@ -1,7 +1,7 @@
 import pytest
 from ratventure.ratventure_functions import *
 
-# TC01 - MAIN MENU
+# TC01 - MAIN MENU ==================================
 def test_mainMenuUI():
     """
     Displays UI for main menu
@@ -21,7 +21,7 @@ def test_mainMenu(monkeypatch):
     value = mainMenu()
     assert value == 1
 
-# TC02 - TOWN MENU 
+# TC02 - TOWN MENU ================================
 def test_townMenuUI():
     value =townMenuUI()
     assert value == "Day 1: You are in a town.\n1) View Character\n2) View Map\n3) Move\n4) Rest\n5) Save Game\n6) Exit Game"
@@ -31,3 +31,38 @@ def test_townMenu(monkeypatch):
     monkeypatch.setattr("builtins.input", lambda _: 1)  
     value = townMenu()
     assert value == 1
+
+
+# TC - OUTDOOR MENU ================================
+def test_outdoorMenuUI():
+    value =outdoorMenuUI()
+    assert value == "1) View Character\n2) View Map\n3) Move\n4) Exit Game"
+
+def test_outdoorMenu(monkeypatch):
+    #input 2
+    monkeypatch.setattr("builtins.input", lambda _: 1)  
+    value = outdoorMenu()
+    assert value == 1
+
+def test_outdoorMenu_error(monkeypatch):
+    #input 6 - should display error message
+    monkeypatch.setattr("builtins.input", lambda _: 6)  
+    value = attackMenu()
+    assert value == "Invalid number. Please try again."
+
+# TC - ATTACK MENU =================================
+def test_attackMenuUI():
+    value =attackMenuUI()
+    assert value == "1) Attack\n2) Run"
+
+def test_attackMenu(monkeypatch):
+    #input 2
+    monkeypatch.setattr("builtins.input", lambda _: 1)  
+    value = attackMenu()
+    assert value == 1
+
+def test_attackMenu_error(monkeypatch):
+    #input 6 - should display error message
+    monkeypatch.setattr("builtins.input", lambda _: 6)  
+    value = attackMenu()
+    assert value == "Invalid number. Please try again."
