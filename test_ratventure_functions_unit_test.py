fb87dd1db50ba8bcd6e129439736139d0c1d6c23
nomzlyt <s10187493@connect.np.edu.sg>
2021-01-28 15:25:32 +0800

    Added Test Script: UTC 15

--- test_ratventure_functions_unit_test.py
+++ test_ratventure_functions_unit_test.py
@@ -1,4 +1,10 @@
 import pytest
 from ratventure.ratventure_functions import *
 
-
+#UTC15 - This test checks if the orb is spawned in any town except the first one.
+def test_orbposition():
+    
+    townPosition = [1,12,22,26,53]
+    value = spawnorb(townPosition)
+    assert value == townPosition
+    assert value != 1
