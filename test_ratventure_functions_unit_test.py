5b1c4c176bf94babfefa231cb0916d5b8fd5e379
nomzlyt <s10187493@connect.np.edu.sg>
2021-01-28 18:50:51 +0800

    Added test Script: UTC16

--- test_ratventure_functions_unit_test.py
+++ test_ratventure_functions_unit_test.py
@@ -1,4 +1,14 @@
 import pytest
 from ratventure.ratventure_functions import *
 
+#UTC 16 - The test checks if the player is holding orb of power ans his stats have been increased by 5.
 
+def test_playerhasorb():
+    player = Player()
+
+    obtainOrb(player)
+
+    assert player.hasOrb == True
+    assert player.maxDamage == 9
+    assert player.minDamage == 7
+    assert player.defence == 6
