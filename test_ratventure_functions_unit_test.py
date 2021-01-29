3bd140fafe0505222bdbdfe1616c9b7d478931c3
Sean Nicholas <seannts@outlook.com>
2021-01-29 18:42:47 +0800

    Added Test Script: UTC16

--- test_ratventure_functions_unit_test.py
+++ test_ratventure_functions_unit_test.py
@@ -1,4 +1,17 @@
 import pytest
 from ratventure.ratventure_functions import *
 
+# UTC16
+def test_rat_king_object():
+    '''
+    This function is to test if the Rat King object is created / initialised as intended to.
+    '''
+    ratKing = createRatKing()
+
+    assert ratKing.name == "Rat King"
+    assert ratKing.minDamage == 8
+    assert ratKing.maxDamage == 12
+    assert ratKing.defence == 5
+    assert ratKing.maxHp == 25
+    assert ratKing.alive == True
 
