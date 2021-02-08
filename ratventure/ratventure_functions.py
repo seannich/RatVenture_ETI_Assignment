2bdeb0d3d4fd9eb365d61b83af357470913feea9
nomzlyt <s10187493@connect.np.edu.sg>
2021-02-08 21:34:57 +0800

    Added ObtainOrb function: Player's stats increase and hasOrb is set to true

--- ratventure/ratventure_functions.py
+++ ratventure/ratventure_functions.py
@@ -18,6 +18,7 @@ class Player:
         self.location = 'You are in a Town'
         self.locationTag = 'H'
         self.combat = False
+        self.hasOrb = False
 
     def herostats(self):
         ''' 
@@ -142,6 +143,11 @@ class Enemy:
         self.hp = self.maxHp
         self.alive = True
 
+def obtainOrb(player):
+    player.hasOrb = True
+    player.minDamage = 7
+    player.maxDamage = 9
+    player.defence = 6
 
 def mapUI(position):
     """
@@ -305,77 +311,4 @@ def attackMenu():
         return "Invalid number. Please try again."
     else:
         return choice
-"""
-def main():
-
-choice ='0'
-while choice =='0':
-    print("Welcome to Ratventure!")
-    print("1) View Character")
-    print("2) View Map")
-    print("3) Move")
-    print("4) Rest")
-    print("5) Save Game")
-    print("6) Exit Game")
-
-
-    choice = input ("Enter choice: ")
-    if choice == "1":
-        print("Enter choice: 1") #Display stats
-        print("The Hero")
-        print(" Damage:" + damage)
-        print("Defence:" + defence)
-        print("     HP:" + hp)
-
-    elif choice == "2": 
-        print("Enter choice: 2") #Display map
-        print("+---+---+---+---+---+---+---+---+ \
-               |H/T|   |   |   |   |   |   |   | \
-               +---+---+---+---+---+---+---+---+ \
-               |   |   |   | T |   |   |   |   | \
-               +---+---+---+---+---+---+---+---+ \
-               |   |   |   |   |   | T |   |   | \
-               +---+---+---+---+---+---+---+---+ \
-               |   | T |   |   |   |   |   |   | \
-               +---+---+---+---+---+---+---+---+ \
-               |   |   |   |   |   |   |   |   | \
-               +---+---+---+---+---+---+---+---+ \
-               |   |   |   |   |   |   |   |   | \
-               +---+---+---+---+---+---+---+---+ \
-               |   |   |   |   | T |   |   |   | \
-               +---+---+---+---+---+---+---+---+ \
-               |   |   |   |   |   |   |   | K | \
-               +---+---+---+---+---+---+---+---+")  #-> x axis
-    elif choice == "3": #Move
-    print("Enter choice: 3")
-    print("W = up; A = left; S = down; D = right")
-
-    elif choice == "4":     
-    print("Enter choice: 4")
-    self.hp == 20
-    print("You are fully healed.")
-    elif choice == "5":
-    print("Enter choice: 5")
-    data = {'health':100, 'gold': 1560, 'name': 'mariano'}
-    print("Game saved.")
-    elif choice == "6":
-    print("Enter choice: 6")
-    sys.exit(0)
-    else:
-        print("Please choose a number from 1 to 6.")
-
- def second_menu():
-     print("This is the second menu")
-
- main()
-
-
-#load save file
- #with open('savefile', 'w') as f:
-    #pickle.dump(data, f)
-
-#with open('savefile') as f:
-    #data = pickle.load(f)
-
-"""
 
